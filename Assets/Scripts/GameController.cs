559c2c084a42eeead2b834e0c8b2bae67681e24b
TheronHarr <59665002+TheronHarr@users.noreply.github.com>
2020-04-04 18:41:33 -0400

    updated for challenge

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject hazard;
+    public GameObject[] hazards;
     public Vector3 spawnValues;
     public int hazardCount;
     public float spawnWait;
@@ -37,6 +37,7 @@ public class GameController : MonoBehaviour
         {
             for (int i = 0; i < hazardCount; i++)
             {
+                GameObject hazard = hazards[Random.Range (0,hazards.Length)];
                 Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
                 Quaternion spawnRotation = Quaternion.identity;
                 Instantiate(hazard, spawnPosition, spawnRotation);
@@ -46,7 +47,7 @@ public class GameController : MonoBehaviour
 
             if(gameOver)
             {
-                restartText.text = "Press 'R' for Restart";
+                restartText.text = "Press 'F' for Restart";
                 restart = true;
                 break;
             }
@@ -61,14 +62,19 @@ public class GameController : MonoBehaviour
 
     void UpdateScore()
     {
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            gameOverText.text = "You Win! Game created by Theron Harrison.";
+            gameOver = true;
+        }
     }
 
     void Update()
     {
         if (restart)
         {
-            if (Input.GetKeyDown(KeyCode.R))
+            if (Input.GetKeyDown(KeyCode.F))
             {
                 SceneManager.LoadScene("SampleScene");
             }
